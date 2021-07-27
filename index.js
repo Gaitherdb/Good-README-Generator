dd3d2ecfdc9e30cadf8cb1756dd12dc3e337547f
David Gaither <gaitherdb@gmail.com>
2021-07-27 19:00:37 -0400

    add questions and writefiles them to a README.md

--- index.js
+++ index.js
@@ -1,13 +1,78 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// create writeFile function using promises instead of a callback function
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'GitHub',
+            message: 'What is your GitHub username?',
+        },
+        {
+            type: 'input',
+            name: 'Email',
+            message: 'What is your email address?',
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: "What is your project's title?",
+        },
+        {
+            type: 'input',
+            name: 'Description',
+            message: 'Write a short description of your project.',
+        },
+        {
+            type: 'list',
+            name: 'License',
+            message: 'What kind of license should your project have?',
+            choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+        },
+        {
+            type: 'input',
+            name: 'Installation',
+            message: 'What command should be run to install dependencies?',
+            default: 'npm i',
+        },
+        {
+            type: 'input',
+            name: 'Usage',
+            message: 'What does the user need to know about using the repo?',
+        },
+        {
+            type: 'input',
+            name: 'Contributing',
+            message: 'What does the user need to know about contributing to the repo?',
+        },
+        {
+            type: 'input',
+            name: 'Tests',
+            message: 'What command should be run to run tests?',
+            default: 'npm test',
+        },
+    ]);
+};
 
-// Function call to initialize app
+const generateREADME = (answers) =>
+    `# ${answers.title}`;
+
+
+
+
+const init = () => {
+    promptUser()
+        .then((answers) => writeFileAsync('README.md', generateREADME(answers)))
+        .then(() => console.log('Successfully wrote to README.md'))
+        .catch((err) => console.error(err));
+};
+
 init();
+
+
