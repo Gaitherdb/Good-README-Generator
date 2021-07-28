0e3e9522e8c30bf1f809c8137abf6c9251393070
David Gaither <gaitherdb@gmail.com>
2021-07-28 18:34:40 -0400

    edit license link and edit install section

---
+++ README.md
@@ -0,0 +1,19 @@
+# title
+  ## ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+  ## Description
+  msdf
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation
+  To install necessary dependencies, run the following command:
+  ```
+  npm i
+  ```
+  
+  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,47 +1,37 @@
-
 function renderLicenseBadge(license) {
-  if (license === 'MIT') {
-    return '## ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)'
-  }
-  else if (license === 'Apache 2.0') {
-    return '## ![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)'
-  }
-  else if (license === 'GPL 3.0') {
-    return '## ![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)'
-  }
-  else if (license === 'BSD 3') {
-    return '## ![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)'
-  }
-  else {
-    return ``
+  if (license === "MIT") {
+    return "## ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
+  } else if (license === "Apache 2.0") {
+    return "## ![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)";
+  } else if (license === "GPL 3.0") {
+    return "## ![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)";
+  } else if (license === "BSD 3") {
+    return "## ![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)";
+  } else {
+    return ``;
   }
 }
 
 function renderLicenseLink(license) {
-  if (license === 'MIT') {
-    return '(https://opensource.org/licenses/MIT)'
-  }
-  else if (license === 'Apache 2.0') {
-    return '(https://opensource.org/licenses/Apache-2.0)'
-  }
-  else if (license === 'GPL 3.0') {
-    return '(https://www.gnu.org/licenses/gpl-3.0)'
-  }
-  else if (license === 'BSD 3') {
-    return '(https://opensource.org/licenses/BSD-3-Clause)'
-  }
-  else {
-    return ``
+  if (license === "MIT") {
+    return "This project is licensed under the terms of the [MIT License](https://opensource.org/licenses/MIT)";
+  } else if (license === "Apache 2.0") {
+    return "This project is licensed under the terms of the [Apache 2.0 License](https://opensource.org/licenses/Apache-2.0)";
+  } else if (license === "GPL 3.0") {
+    return "This project is licensed under the terms of the [GPL 3.0 License](https://www.gnu.org/licenses/gpl-3.0)";
+  } else if (license === "BSD 3") {
+    return "This project is licensed under the terms of the [BSD 3 License](https://opensource.org/licenses/BSD-3-Clause)";
+  } else {
+    return ``;
   }
 }
 
 function renderLicenseSection(license) {
-  if (license === 'None') {
-    return ``
-  }
-  else {
+  if (license === "None") {
+    return ``;
+  } else {
     return `
-  * [License](#license)`
+  * [License](#license)`;
   }
 }
 
@@ -59,6 +49,13 @@ function generateMarkdown(data) {
   * [Contributing](#contributing)
   * [Tests](#tests)
   * [Questions](#questions)
+  
+  ## Installation
+  To install necessary dependencies, run the following command:
+  \`\`\`
+  ${data.Installation}
+  \`\`\`
+  
   `;
 }
 
